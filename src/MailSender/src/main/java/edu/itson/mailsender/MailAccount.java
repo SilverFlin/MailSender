ff11860acee6c3534acbbd8a98dc2183e6adfd3e
JIVB <darksamus0997@hotmail.com>
2023-09-24 21:50:24 -0700

    feat: caso de uso 1 agregado AR-10

---
+++ src/MailSender/src/main/java/edu/itson/mailsender/MailAccount.java
@@ -0,0 +1,14 @@
+
+package edu.itson.mailsender;
+
+/**
+ * Clase que representa una direccion de correo
+ * @author José Iván Vázquez Brambila
+ * @version domingo 24 de septiembre 2023
+ */
+public class MailAccount {
+    //Cadena que represnta un correo electronico
+    public String email;
+    //Objeto que representa el proveeder del servicio
+    public MailServer provider;
+}
---
+++ src/MailSender/src/main/java/edu/itson/mailsender/MailServer.java
@@ -0,0 +1,15 @@
+
+package edu.itson.mailsender;
+
+
+/**
+ * Clase que representa el Servicio de correo
+ * @author José Iván Vázquez Brambila
+ * @version domingo 24 de septiembre 2023
+ */
+public class MailServer {
+    //Cadena que representa el nombre del Servidor
+    public String name;
+    //Lista de objetos de protocolos 
+    public   java.util.ArrayList<Protocol> List;
+}
